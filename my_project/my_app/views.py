25f31c19486bfcbaa6ef168ced2164fa3fe5063e
amarsengar <amarsengar2020@gmail.com>
2024-04-29 20:57:11 +0530

    views updated

--- my_project/my_app/views.py
+++ my_project/my_app/views.py
@@ -8,7 +8,6 @@ import os
 def my_view(request):
     
     terminal = os.system("start cmd /K dir")
-    # terminal =os.system("gnome-terminal -e 'bash -c \"sudo apt-get update; exec bash\"'")
     
 
     return render(request,template_name='index.html')
